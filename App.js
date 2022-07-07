3904668b9b243ba96e8da8ad3f45f26ad313ab9c
fu860114 <fu860114@gmail.com>
2022-07-07 11:25:42 +0800

    useEffect & counter

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [name, setName]=useState('')
+const [count, setCount]=useState(0)
+
+useEffect(()=>{
+  const myCount =setInterval(()=>{
+    setCount(count+1)
+  },1000)
+  return()=>{
+    clearInterval(myCount)
+  }
+},[count])
+  // useEffect(()=>{
+  //   return  // unmount effect
+  //   console.log('useEffect',name)
+  // },[name])
+
+// const changeName=()=>{
+//   console.log('changeName',name)
+//   setName('jacabe')
+//   console.log('after changeName',name)
+// }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* {console.log('render',name)} */}
+    <Text>{count}</Text>
+    {console.log('render',count)}
+    {/* <Button
+    title={'change name'}
+    onPress={()=>changeName()}
+    /> */}
     </View>
   );
 }
